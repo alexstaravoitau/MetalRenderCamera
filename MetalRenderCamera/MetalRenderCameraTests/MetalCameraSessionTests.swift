571105c564af7d7cd7941e846b0709aa41b3cd45
Alex Staravoitau <alex.staravoitau@gmail.com>
2016-07-25 20:50:29 +0100

    Add initial tests.

---
+++ MetalRenderCamera/MetalRenderCameraTests/MetalCameraSessionTests.swift
@@ -0,0 +1,27 @@
+//
+//  MetalCameraSessionTests.swift
+//  MetalRenderCamera
+//
+//  Created by Alex Staravoitau on 25/07/2016.
+//  Copyright © 2016 Old Yellow Bricks. All rights reserved.
+//
+
+import XCTest
+
+class MetalCameraSessionTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+    func testCameraAccess() {
+        
+    }
+
+}
