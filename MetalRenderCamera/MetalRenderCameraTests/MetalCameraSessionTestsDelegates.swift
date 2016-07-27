05bc8a90cb665063eec7b79080e712a02c4066ec
Alex Staravoitau <alex.staravoitau@gmail.com>
2016-07-27 21:38:06 +0100

    Add stub delegates for testing.
    Add stub delegates that would intercept camera session delegate callbacks and report corresponding `XCTest` expectations.

---
+++ MetalRenderCamera/MetalRenderCameraTests/MetalCameraSessionTestsDelegates.swift
@@ -0,0 +1,48 @@
+//
+//  MetalCameraSessionTestsDelegates.swift
+//  MetalRenderCamera
+//
+//  Created by Alex Staravoitau on 27/07/2016.
+//  Copyright © 2016 Old Yellow Bricks. All rights reserved.
+//
+
+import XCTest
+import Metal
+
+@testable import Metal_Camera
+
+/// This class is acting as a `MetalCameraSessionDelegate` that tracks the first error received from the camera session and reports it with a `XCTestExpectation`.
+internal final class ErrorTrackingDelegate: MetalCameraSessionDelegate {
+
+    /// Stores the camera session error, so that it's available for a test
+    var error: MetalCameraSessionError?
+    /// Expectation that is waiting for the delegate
+    var expectation: XCTestExpectation?
+
+    internal func metalCameraSession(session: MetalCameraSession, didReceiveFrameAsTextures: [MTLTexture], withTimestamp: Double) { }
+
+    internal func metalCameraSession(session: MetalCameraSession, didUpdateState: MetalCameraSessionState, error newError: MetalCameraSessionError?) {
+        guard let expectation = expectation where (error == nil && newError != nil) else { return }
+
+        error = newError
+        expectation.fulfill()
+    }
+}
+
+/// This class is acting as a `MetalCameraSessionDelegate` that tracks the first state received from the camera session and reports it with a `XCTestExpectation`.
+internal final class StateTrackingDelegate: MetalCameraSessionDelegate {
+
+    /// Stores the camera session state, so that it's available for a test
+    var state: MetalCameraSessionState?
+    /// Expectation that is waiting for the delegate
+    var expectation: XCTestExpectation?
+
+    internal func metalCameraSession(session: MetalCameraSession, didReceiveFrameAsTextures: [MTLTexture], withTimestamp: Double) { }
+
+    internal func metalCameraSession(session: MetalCameraSession, didUpdateState newState: MetalCameraSessionState, error: MetalCameraSessionError?) {
+        guard let expectation = expectation where state == nil else { return }
+
+        state = newState
+        expectation.fulfill()
+    }
+}
