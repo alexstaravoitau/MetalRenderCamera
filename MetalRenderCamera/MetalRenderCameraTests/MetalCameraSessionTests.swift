cbb762dd0412dbd81c5a543fe07251ad63387f70
Alex Staravoitau <alex.staravoitau@gmail.com>
2016-07-27 21:38:45 +0100

    Add unit tests.
    Add basic `XCTestCase`s for getting hardware access. More or less as far as you can go in a project like this one.

--- MetalRenderCamera/MetalRenderCameraTests/MetalCameraSessionTests.swift
+++ MetalRenderCamera/MetalRenderCameraTests/MetalCameraSessionTests.swift
@@ -7,21 +7,95 @@
 //
 
 import XCTest
+import AVFoundation
+
+@testable import Metal_Camera
 
 class MetalCameraSessionTests: XCTestCase {
 
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
+    /**
+     Test that camera session reports a corresponding error to its delegate if there is no access to hardware.
+     */
+    func testErrorWithoutDeviceAccess() {
+        /// A class faking `MetalCameraCaptureDevice` that would mock access requests and devices availability
+        class StubCaptureDevice: MetalCameraCaptureDevice {
+            override func requestAccessForMediaType(mediaType: String!, completionHandler handler: ((Bool) -> Void)!) { handler(false) }
+        }
+
+        let delegate = ErrorTrackingDelegate()
+        let session = MetalCameraSession(delegate: delegate)
+        let expectation = expectationWithDescription("MetalCameraSession calls delegate with an updated state and error.")
+        delegate.expectation = expectation
+        session.captureDevice = StubCaptureDevice()
+        session.start()
+
+        waitForExpectationsWithTimeout(1) { error in
+            if let error = error {
+                /**
+                 Apparently the error was never reported and the expectation timed out.
+                 */
+                XCTFail(error.description)
+            }
+
+            XCTAssert(delegate.error == .NoHardwareAccess, "Camera session reported a inconsistent error.")
+        }
     }
 
-    func testCameraAccess() {
-        
+    /**
+     Test that camera session reports a corresponding state to its delegate if access to hardware was successfully granted.
+     */
+    func testStateWithDeviceAccess() {
+        /// A class faking `MetalCameraCaptureDevice` that would mock access requests and devices availability
+        class StubCaptureDevice: MetalCameraCaptureDevice {
+            override func requestAccessForMediaType(mediaType: String!, completionHandler handler: ((Bool) -> Void)!) { handler(true) }
+        }
+
+        let delegate = StateTrackingDelegate()
+        let session = MetalCameraSession(delegate: delegate)
+        let expectation = expectationWithDescription("MetalCameraSession calls delegate with an updated state and error.")
+        delegate.expectation = expectation
+        session.captureDevice = StubCaptureDevice()
+        session.start()
+
+        waitForExpectationsWithTimeout(1) { error in
+            if let error = error {
+                /**
+                 Apparently the status was never reported and the expectation timed out.
+                 */
+                XCTFail(error.description)
+            }
+
+            XCTAssert(delegate.state == .Ready)
+        }
+    }
+
+    /**
+     Test that camera session reports a corresponding error to its delegate if there is no required hardware available.
+     */
+    func testErrorWithNoHardwareAvailable() {
+        /// A class faking `MetalCameraCaptureDevice` that would mock access requests and devices availability
+        class StubCaptureDevice: MetalCameraCaptureDevice {
+            override func requestAccessForMediaType(mediaType: String!, completionHandler handler: ((Bool) -> Void)!) { handler(true) }
+            override func deviceWithMediaType(mediaType: String, position: AVCaptureDevicePosition) -> AVCaptureDevice? { return nil }
+        }
+
+        let delegate = ErrorTrackingDelegate()
+        let session = MetalCameraSession(delegate: delegate)
+        let expectation = expectationWithDescription("MetalCameraSession calls delegate with an updated state and error.")
+        delegate.expectation = expectation
+        session.captureDevice = StubCaptureDevice()
+        session.start()
+
+        waitForExpectationsWithTimeout(1) { error in
+            if let error = error {
+                /**
+                 Apparently the error was never reported and the expectation timed out.
+                 */
+                XCTFail(error.description)
+            }
+
+            XCTAssert(delegate.error == .RequestedHardwareNotFound, "Camera session reported a inconsistent error.")
+        }
     }
 
 }
